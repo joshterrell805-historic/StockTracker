e6a6fa0abf82df05be384069f57499c9d41429bc
Josh Terrell <joshterrell805@gmail.com>
2015-09-14 14:44:46 -0700

    remove all but volume and last trade

--- src/migrations/1442265134.sql
+++ src/migrations/1442265134.sql
@@ -1,3 +1,7 @@
+alter table quotes drop column ask;
 alter table quotes drop column ask_rt;
+alter table quotes drop column ask_size;
+alter table quotes drop column bid;
 alter table quotes drop column bid_rt;
+alter table quotes drop column bid_size;
 alter table quotes drop column last_trade_rt;
